4b7c886cdb674e54e3d2a74def5a4b300fb6377a
Ismail Kilic <kilic.2000@hotmail.com>
2023-12-28 14:33:41 +0300

    function, arrow func, setTimeout, callback, promise

--- _5_Javascript/template.js
+++ _5_Javascript/template.js
@@ -118,68 +118,68 @@
 
 /////////////////////////////////////////////////
 // Sayısal İşlemler
-let number=44;
-number=44.4252;
-console.log(number);
-console.log("Number: "+ Number(number));
-console.log("parseInt: "+parseInt(number));
-console.log("parseFloat: "+parseFloat(number));
+// let number=44;
+// number=44.4252;
+// console.log(number);
+// console.log("Number: "+ Number(number));
+// console.log("parseInt: "+parseInt(number));
+// console.log("parseFloat: "+parseFloat(number));
 // NaN 
 // isNaN
-console.log(typeof number);
+// console.log(typeof number);
 
-let bilimsel1=1400000;
-console.log(bilimsel1);
+// let bilimsel1=1400000;
+// console.log(bilimsel1);
 
-let bilimsel2=14E+5;
-console.log(bilimsel2);
+// let bilimsel2=14E+5;
+// console.log(bilimsel2);
 
-let bilimsel3=14E-5;
-console.log(bilimsel3);
+// let bilimsel3=14E-5;
+// console.log(bilimsel3);
 
 // binary(2)
-let binary=0b01010;
-console.log(binary);
+// let binary=0b01010;
+// console.log(binary);
 
 // octal(8)
-let octal=0o7523;
-console.log(octal);
+// let octal=0o7523;
+// console.log(octal);
 
 // hexadecimal(16) 0123456789abcdef
-let hexadecimal=0xff09;
-console.log(hexadecimal);
+// let hexadecimal=0xff09;
+// console.log(hexadecimal);
 
-const number2=56.123456789;
-console.log(number2);
-console.log(typeof number2);
-console.log(number2.toFixed(3));
-console.log(number2.toPrecision(3));
+// const number2=56.123456789;
+// console.log(number2);
+// console.log(typeof number2);
+// console.log(number2.toFixed(3));
+// console.log(number2.toPrecision(3));
 
 /////////////////////////////////////////////////
 // Metinsel işlemler
 
-let vocabulary = " js Öğreniyorum js ";
-console.log(vocabulary);
-console.log(vocabulary.length);
-console.log(vocabulary.trim().length);
+// let vocabulary = " js Öğreniyorum js ";
+// console.log(vocabulary);
+// console.log(vocabulary.length);
+// console.log(vocabulary.trim().length);
 
-console.log(vocabulary.toLowerCase());
-console.log(vocabulary.toUpperCase());
+// console.log(vocabulary.toLowerCase());
+// console.log(vocabulary.toUpperCase());
 
-console.log(vocabulary.substring(5));
-console.log(vocabulary.substring(5,8)); // 5<=X<=8-1
+// console.log(vocabulary.substring(5));
+// console.log(vocabulary.substring(5,8)); // 5<=X<=8-1
 
-console.log(vocabulary.charAt(0)); // 0.karakter
-console.log(vocabulary.indexOf("js")); 
-console.log(vocabulary.lastIndexOf("js")); 
-console.log(vocabulary.includes("js")); 
+// console.log(vocabulary.charAt(0)); // 0.karakter
+// console.log(vocabulary.indexOf("js")); 
+// console.log(vocabulary.lastIndexOf("js")); 
+// console.log(vocabulary.includes("js")); 
 
-console.log(vocabulary.replace("js","javascript"));
-console.log(vocabulary.concat("-sona")); // concat: sonuna ekler.
-console.log(vocabulary+"-sona");
+// console.log(vocabulary.replace("js","javascript"));
+// console.log(vocabulary.concat("-sona")); // concat: sonuna ekler.
+// console.log(vocabulary+"-sona");
 
-console.log(vocabulary.startsWith("js")); // js ile başlıyorsa true yada false döner. Boolean
-console.log(vocabulary.endsWith("js")); // js ile bitiyorsa true yada false döner. Boolean
+// console.log(vocabulary.startsWith("js")); // js ile başlıyorsa true yada false döner. Boolean
+// console.log(vocabulary.endsWith("js")); // js ile bitiyorsa true yada false döner. Boolean
 
 // ÖDEV // kullanıcı tarafından girilen bir kelimenin (prompt)
 // let userData= prompt("Lütfen bir şeyler yazınız")  ;
@@ -195,4 +195,271 @@ console.log(vocabulary.endsWith("js")); // js ile bitiyorsa true yada false dön
 // S-8) ilk harf nedir  ? (charAt())
 // S-9) girdiğiniz kelimenin sonuna "-ben js öğreniyorum" (concat())
 // S-10) girdiğiniz 0 ile 4 arasındaki indexi ekranda gösterin? (subString())
+
 ///////////////////////////////////////////////////////////////
+// Function
+// 1 parametresizRetursuz
+// function parametresizRetursuz(){
+//     console.log("parametresiz Retursuz");
+// }
+// parametresizRetursuz();
+
+// 2 parametreliRetursuz
+// function parametreliRetursuz(number){
+//     console.log("parametreli Retursuz "+(Math.sqrt(number)));
+// }
+// parametreliRetursuz(25);
+
+// 3 parametresizReturlu
+// function parametresizReturlu(){
+//     return Math.pow(2,5);
+// }
+// let data3=parametresizReturlu();
+// console.log(data3);
+
+// 4 parametreliReturlu
+// function parametreliReturlu(alt,ust){
+//     return Math.pow(alt,ust);
+// }
+// let data4=parametreliReturlu(2,5);
+// console.log(data4);
+
+///////////////////////////////////////////////////////////////
+// Immedia Function
+// (function () {  
+//     console.log('Immedia Function Anonymous');
+// })
+
+// (() => {
+//     console.log("Immedia Function Arrow")
+// });
+
+// Function
+// function normFuction() {
+//     console.log("normFuction");
+// }
+// normFuction()
+
+// Anonymous Function
+// let anonymousfunction = function () {
+//     console.log("anonymous function");
+// }
+// anonymousfunction()
+
+// Arrows Function
+// let arrowfunction = (data) => {
+//     console.log("arrow function " + data);
+// }
+// arrowfunction("asd")
+
+///////////////////////////////////////////////////////////////
+// Conditional 
+// let conditional = (data) => {
+//     let number = 5;
+//     if (number > 0) {
+//         console.log("pozitiftir");
+//     } else
+//         console.log("negatif");
+// }
+// conditional();
+
+// let ternary = (data) => {
+//     let number = 5;
+//     const result = (number > 0) ? "pozitiftir" : "negatif";
+//     console.log(result);
+// }
+// ternary()
+
+// Conditional 
+// let conditional2 = (data) => {
+//     let number = parseInt(Number("4"));
+//     if (number == 0) {
+//         console.log("0");
+//     } else if (number == 1) {
+//         console.log("1");
+//     } else if (number == 2) {
+//         console.log("2");
+//     } else if (number == 3) {
+//         console.log("3");
+//     } else if (number === 4) {
+//         console.log("4");
+//     } else if (number == 5) {
+//         console.log("5");
+//     } else {
+//         console.log("1<=X<=5 dışında");
+//     }
+// }
+// conditional2();
+
+// let conditional3 = (data) => {
+//     let number = parseInt(Number("4"));
+//     switch (number) {
+//         case 0:
+//             console.log("0");
+//             break;
+//         case 1:
+//             console.log("1");
+//             break;
+//         case 3:
+//             console.log("3");
+//             break;
+//         case 2:
+//             console.log("2");
+//             break;
+//         case 4:
+//             console.log("4");
+//             break;
+//         case 5:
+//             console.log("5");
+//             break;
+//         default:
+//             console.log("1<=X<=5 dışında");
+//             break;
+//     }
+// }
+// conditional3();
+
+///////////////////////////////////////////////////////////////
+// Loop
+// let forLoop = () => {
+//     for (let i = 1; i <=5; i++) {
+//         window.document.writeln("i: "+i+"<br/>");
+//     }
+    //for(;;){}
+// }
+// forLoop();
+// window.document.writeln("*************** <br/>");
+
+// let whileLoop = () => {
+//     let i = 1;
+//     while ( i <=5) {
+//         window.document.writeln("i: "+i+"<br/>");
+//         i=i+1;
+//     }
+    //while(true){}
+// }
+// whileLoop();
+
+// window.document.writeln("*************** <br/>");
+// let doWhileLoop = () => {
+//     let i = 1;
+//     do{
+//        window.document.writeln("i: "+i+"<br/>");
+//         i=i+1; 
+//     }
+//     while ( i <=5) ;
+    //while(true){}
+// }
+// doWhileLoop();
+
+///////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////
+// Exception Handling
+// const exception = () => {
+//     try {
+//         alertx("Alert");
+//     } catch (err) {
+//         console.error(err);
+        // throw
+        // throw new Error("zorunlu istisna yaptım");
+    // } finally {
+    //     console.log("Ekranda göster Mutlaka");
+    // }
+    // console.log("son satır");
+// }
+//exception();
+
+
+///////////////////////////////////////////////////////////////
+// debug
+// debugger
+// let debug = () => {
+    //debugger
+    // for (let i = 1; i <=5; i++) {
+        //window.document.writeln("i: "+i+"<br/>");
+    //     console.log("i: "+i+"<br/>");
+    // }
+    //debugger
+// }
+// debug();
+
+///////////////////////////////////////////////////////////////
+// setTimeout(): sadece 1 kere çalışır ve sonra durur.
+let setTimeoutFunction = () => {
+    setTimeout(() => {
+        console.log("setTimeoutFunction 5 saneiye sonra çalıştı");
+    }, 5000);
+}
+// setTimeoutFunction();
+
+// setInterval():  ssürekli çalışır
+let setIntervalFunction = () => {
+    setInterval(() => {
+        console.log("setInterval 2 saneiye sonra çalışsın ve devam etsin");
+    }, 1000);
+}
+//setIntervalFunction();
+
+// setTimeout ve setInterval function beraber kullanmak
+let setTimeoutAndIntervalFunction = () => {
+
+    //Function
+    const clockData = () => {
+        console.log("1.saniye");
+    }
+
+    // setInterval function: Sürekli çalışsın
+    let intervalID = setInterval(clockData, 1000);
+
+    // 1 dakika sonra dursun.
+    // setTimeout(): sadece 1 kere çalışır ve sonra durur.
+    // 60*60*24*365
+    const time = parseInt(60);
+    setTimeout(() => {
+        clearInterval(intervalID);
+        console.log(time + " zaman sonra durdu.");
+    }, 1 * 1000 * time);
+}
+//setTimeoutAndIntervalFunction();
+
+///////////////////////////////////////////////////////////////
+// Senkron(single thread: process) - Asenkron nedir((multiple thread: process)) ?
+// Javascript tabiatı gereği Senkron mu ? Asenkron mu ?
+///////////////////////////////////////////////////////////////
+
+// call back function
+const callBackFunction = () => {
+    // 1.Function
+    let birinci = (data) => {
+        return Math.sqrt(data)
+    }
+
+    // 2.Function
+    let ikinci = (xyz) => {
+        //let userData=  parseInt(prompt("Lütfen bir sayı giriniz"));
+        let userData = 25
+        const result = xyz(userData);
+        console.log(result);
+    }
+    ikinci(birinci);
+}
+// callBackFunction()
+
+// promise
+const promiseFunction = () => {
+    const isLogin = true;
+    const isCreated = 201;
+    let pro = new Promise((resolve, reject) => {
+        if (isLogin && isCreated==201)
+            resolve("olumlu sonuç");
+        else
+            reject("olumsuz sonuç");
+    }).then(() => { // new Promise eğer sonuç geldiyse then çalışsın
+        console.log("Then");
+    }).catch(() => { // new Promise eğer sonuç olumsuzsa catch çalışsın
+        console.error("catch");
+    });
+    console.log(pro);
+}
+promiseFunction()
