0e0b64aa4dd52fb34df9afd5157c16845bc5dd6a
Ismail Kilic <kilic.2000@hotmail.com>
2024-01-02 15:38:15 +0300

    Object, DOM, Event, addEventListener

--- _5_Javascript/template.js
+++ _5_Javascript/template.js
@@ -691,16 +691,119 @@ let objectData = () => {
             age: 20,
         },
         result:function(){
-            console.log("Function called");
-            // return surname+" "+this.isLogin;
+            console.log(this.surname+" Function çalıştı");
+            //console.log(person.surname+"Function çalıştı");
+            //return surname+" "+isLogin;
         }
     }
+    console.log("*********************");
     console.log(person);
+    console.log("*********************");
+    console.log(person.isLogin);
+    console.log(typeof person.isLogin);
+    console.log("*********************");
     console.log(person.name);
-    console.log(person["name"]);
+    console.log(person["name"]); //nested calling
     console.log(person.language.name);
-    console.log(person.language["name"]);
     person.result();
+
+    // object Literal
+    person.number=44;
+    console.log(person.number);
+
+    // diziyi => String'e (toString() veya join)
+    console.log( person.array );
+    console.log( typeof person.array );
+    console.log( typeof person.language );
+    let arrayToString=person.array.toString();
+    console.log(arrayToString);
+    let arrayToString2=person.array.join(",");
+    console.log(arrayToString2);
+
+    // String'i => Diziye çevir (split())
+    let stringToArray=arrayToString.split(",");
+    console.log(stringToArray);
+
+    // JSON toString, parse 
+    let objectToString = JSON.stringify(person);
+    console.log(objectToString);
+    console.log(objectToString.substring(0, 3));
+
+    const data = JSON.parse(objectToString);
+    console.log(data);
+
+    // Object Constructor 
+    // {[a,b,c,d] , [{},{}]}
+    // Object ProtoType
+    // Object hasOwnProperty
+    // Object Create
+
+    // built-in-constructor
+    String.prototype.usluSayi=function(data){
+        return Math.pow(data,2);
+    }
+    console.log("".usluSayi(5));
 }
 
-objectData();
+// objectData();
+
+
+// Object
+let objectData2 = () => {
+    let Student=function(adi,soyadi,numara){
+        this.adi=adi;
+        this.soyadi=soyadi;
+        this.numara=numara;
+        console.log(this);
+    };
+
+    Student.prototype.getAdi=function(){
+        return this.adi;
+    }
+
+    let studentInstance= new Student("adi22","soyadı22",22);
+    //console.log(studentInstance);
+    console.log(studentInstance.soyadi);
+
+   // let studentInstance2= Object.create(Student);
+   // console.log(studentInstance2);
+
+       // built-in-constructor
+       String.prototype.usluSayi=function(data){
+        return Math.pow(data,2);
+    }
+    console.log("".usluSayi(5));
+    
+}
+//objectData2();
+
+let domFunction1=()=>{
+   console.log("çalıştı"); 
+
+   // GET
+   const paragraf=document.getElementById("parag_id");
+   //const paragraf=document.getElementsByClassName("parag_class")[0];
+   //const paragraf=document.getElementsByName("parag_name")[0];
+   //const paragraf=document.getElementsByTagName("p")[1];
+
+   // Dom Html, Text
+   //paragraf.innerHTML="<b><mark>Değiştirdim</mark></b>";
+   paragraf.innerText="Değiştirdim";
+
+   // Dom Css
+   paragraf.style.color="red";
+   paragraf.style.backgroundColor="black";
+   paragraf.style.padding="2rem";
+}
+
+let domFunction2=()=>{
+    // GET
+    const paragraf=document.getElementById("parag_id");
+    paragraf.addEventListener("click",()=>{
+        console.log("add Event Listener Çalıştı");
+        setTimeout(()=>{
+            alert("add Event Listener Çalıştı");
+        },2000);
+    });
+ }
+ domFunction2();
