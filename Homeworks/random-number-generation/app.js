bd53e7a6ac432ae20197123ccc029ad8966737db
Ismail Kilic <kilic.2000@hotmail.com>
2024-01-03 20:57:48 +0300

    Basit revize

--- Homeworks/random-number-generation/app.js
+++ Homeworks/random-number-generation/app.js
@@ -4,6 +4,5 @@ const rastgeleSayiEl = document.querySelector('#rastgeleSayi');
 sayiGetirBtn.addEventListener("click", ()=>{
     let rastgeleSayi = Math.floor(Math.random() * 9) + 1;
 
-    // Rastgele sayıyı ekrana yazdırma
     rastgeleSayiEl.innerText = "Rastgele Sayı: " + rastgeleSayi;
   });
